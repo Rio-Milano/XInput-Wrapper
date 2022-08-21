bd05f3ef9f717d91987444d5a3a99b25f33f2271
Rio Milano <A0040053@live.tees.ac.uk>
2022-08-21 01:10:01 +0100

    update

--- XInput Integration/Source.cpp
+++ XInput Integration/Source.cpp
@@ -22,7 +22,7 @@ int main()
 		{
 			if (controller->GetIsKeyPressed(XINPUT_GAMEPAD_A))
 			{
-				controller->SetVibrationState(controller->GetLeftTrigger() * 65535, controller->GetRightTrigger() * 65535);
+				controller->SetVibrationState(static_cast<WORD>(controller->GetLeftTrigger() * 65535.f), static_cast<WORD>(controller->GetRightTrigger() * 65535.f));
 			}
 			else
 				controller->SetVibrationState(0, 0);
